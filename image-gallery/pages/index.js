c49c057531608679a89db7a61bb0ffa11ce962e3
nasoviva <nasoviva@gmail.com>
2024-09-27 15:16:46 -0300

    feat: input are ready

--- image-gallery/pages/index.js
+++ image-gallery/pages/index.js
@@ -1,59 +1,78 @@
-const per_page = 12;
-const page = 1;
-const extras = "url_m";
-let description = "spring";
-const accessKey = 'EadTIY20XojISltGLBgtmWXeD92Et1BTfoHnsKsSyZk';
-const arrayOfImages = [];
+document.addEventListener('DOMContentLoaded', function () {
+  const per_page = 12;
+  const page = 1;
+  const extras = 'url_m';
+  let description = 'spring';
+  const accessKey = 'EadTIY20XojISltGLBgtmWXeD92Et1BTfoHnsKsSyZk';
+  const arrayOfImages = [];
+  const photoContainer = document.querySelectorAll('.img-div');
+  const searchInput = document.querySelector('.search');
+  const searchSvg = document.querySelector('.search-svg');
+  const clearIcon = document.querySelector('.clear');
 
-function getApiUrl() {
-  return `https://api.unsplash.com/search/photos?query=${description}&per_page=${per_page}&page=${page}&extras=${extras}&&client_id=${accessKey}`;
-}
+  //Functions for get images
 
-const photoContainer = document.querySelector('.main-container');
-
-function populateImages(images) {
-    photoContainer.innerHTML = '';
-    for (let i = 0; i < images.length; i++) {
-        const div = document.createElement('div');
-        div.className = 'img-div';
-        div.style.backgroundImage = `url(${images[i]})`;
-        photoContainer.appendChild(div);
-    }
-}
-
-async function getData() {
-  const url = getApiUrl();
-  const res = await fetch(url);
-  if (!res.ok) {
-    console.error("Error");
+  function getApiUrl() {
+    return `https://api.unsplash.com/search/photos?query=${description}&per_page=${per_page}&page=${page}&extras=${extras}&&client_id=${accessKey}`;
   }
-  const data = await res.json();
-  arrayOfImages.length = 0; 
-  if (data.results && data.results.length > 0) {
-    for (let i = 0; i < data.results.length; i++) {
+
+  async function getData() {
+    const url = getApiUrl();
+    const res = await fetch(url);
+    if (!res.ok) {
+      console.error('Error');
+    }
+    const data = await res.json();
+    arrayOfImages.length = 0;
+    if (data.results && data.results.length > 0) {
+      for (let i = 0; i < data.results.length; i++) {
         arrayOfImages.push(data.results[i].urls.regular);
+      }
+      populateImages(arrayOfImages);
+    } else {
+      console.error('Error');
     }
-    populateImages(arrayOfImages);
-  } else {
-    console.error("Error");
   }
-}
+  searchInput.focus();
+  getData();
 
-getData();
-
-
-//Click handlers for the input
-document.querySelector('.search').addEventListener('input', (e) => {
-  description = e.target.value.trim();
-  if (description.length === 0) {
-    description = 'spring';
+  // Function for populate images
+  function populateImages(images) {
+    for (let i = 0; i < images.length; i++) {
+      photoContainer[i].style.backgroundImage = `url(${images[i]})`;
+    }
   }
-});
 
-document.querySelector('.search').addEventListener('keydown', (e) => {
-  if (e.key === 'Enter') {
+  //Functios for input elements
+  function styleInput() {
+    searchInput.style.boxShadow = '0 0 0.5rem 0.1rem #9a2d2f';
+    setTimeout(function () {
+      searchInput.style.boxShadow = '';
+    }, 300);
+  }
+
+  function clearInput() {
+    searchInput.value = '';
+    clearIcon.style.display = 'none';
+    description = '';
+  }
+
+  //Handlers for the input elements
+  searchInput.addEventListener('input', (event) => {
+    description = event.target.value.trim();
+    clearIcon.style.display = description ? 'block' : 'none';
+  });
+
+  searchInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
       getData();
-  }
-});
+      searchInput.blur();
+    }
+  });
 
-document.querySelector('.search-svg').addEventListener("click", getData);
+  searchSvg.addEventListener('click', getData);
+
+  searchSvg.addEventListener('click', styleInput);
+
+  clearIcon.addEventListener('click', clearInput);
+});
